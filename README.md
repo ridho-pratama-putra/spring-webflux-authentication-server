cbdbb6b11bda39270b1d96a3ec98762d0ef4bf37
ridho <ridhopratamavd@gmail.com>
2023-10-10 19:13:25 +0700

    WIP: setup openID

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# this project was delayed due to 
+### https://stackoverflow.com/questions/72978587/reactive-spring-authorization-server/72979388#72979388
+
+
 spring webflux as auth server.
 
 
@@ -6,3 +10,17 @@ walaupun /login dan /logout harus di define secara manual, tapi kenyataan nya /l
 urutan filter security dulu baru kemudian HTTP method validation
 onAuthenticationSuccess will only called when header authorization have bearer value
 return chain.filter(exchange).contextWrite(ReactiveSecurityContextHolder.withAuthentication(authentication));
+
+spring:
+  security:
+    oauth2:
+      authorizationserver:
+        client:
+          client-1:
+            registration:
+              client-id: "admin-client"
+              # the client secret is "secret" (without quotes)
+              client-secret: "{bcrypt}$2a$10$jdJGhzsiIqYFpjJiYWMl/eKDOd8vdyQis2aynmFN0dgJ53XvpzzwC"
+              client-authentication-methods: "client_secret_basic"
+              authorization-grant-types: "client_credentials"
+              scopes: "user.read,user.write"
--- src/main/java/com/example/springwebfluxauthenticationserver/configurations/SecurityConfiguration.java
+++ src/main/java/com/example/springwebfluxauthenticationserver/configurations/SecurityConfiguration.java
@@ -1,14 +1,42 @@
 package com.example.springwebfluxauthenticationserver.configurations;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+import java.text.ParseException;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.bouncycastle.util.io.pem.PemObject;
+import org.bouncycastle.util.io.pem.PemReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
+
+import com.nimbusds.jose.KeySourceException;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSelector;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,13 +46,16 @@ import lombok.RequiredArgsConstructor;
 public class SecurityConfiguration {
     Logger logger = LoggerFactory.getLogger(SecurityConfiguration.class);
 
+    private final String PUBLIC_KEY_PATH = "keys/public.pem";
     private final CustomAuthenticationManager customAuthenticationManager;
     private final CustomAuthenticationConverter customAuthenticationConverter;
     private final CustomAuthenticationFailureHandler customAuthenticationFailureHandler;
     private final CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
 
+    private final DataSource dataSource;
+
     @Bean
-    public AuthenticationWebFilter authenticationWebFilter() {
+    AuthenticationWebFilter authenticationWebFilter() {
         AuthenticationWebFilter filter = new AuthenticationWebFilter(customAuthenticationManager);
         filter.setServerAuthenticationConverter(customAuthenticationConverter);
 
@@ -37,17 +68,81 @@ public class SecurityConfiguration {
 
     @Bean
     SecurityWebFilterChain securityFilterChain(ServerHttpSecurity httpSecurity) throws Exception {
+        OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource());
         httpSecurity
             .csrf(customizer -> customizer.disable())
             .authorizeExchange(customizer -> customizer
                 // .pathMatchers("/login").permitAll()
-                .pathMatchers("/v3/api-docs/*", "/v3/api-docs", "/webjars/swagger-ui/*").permitAll()
+                .pathMatchers("/v3/api-docs/*", "/v3/api-docs", "/webjars/swagger-ui/*", "/.well-known/*", "/oauth2/jwks").permitAll()
                 .anyExchange().authenticated()
             )
             .addFilterAt(authenticationWebFilter(), SecurityWebFiltersOrder.AUTHENTICATION)
             .formLogin(customizer -> customizer.disable())
-            ;
+            .oauth2ResourceServer(resourceServer -> {
+                resourceServer.jwt(Customizer.withDefaults());
+            });
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    ReactiveJwtDecoder jwtDecoder() throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+        // KeyFactory factory = KeyFactory.getInstance("RSA");
+        // ClassPathResource publicKeyResource = new ClassPathResource(PUBLIC_KEY_PATH);
+
+        // disini kayanya yg kurang proper, jadi bikin invalid cert
+        // InputStream inputStream = publicKeyResource.getInputStream();
+
+        // byte[] content = inputStream.readAllBytes();
+        // X509EncodedKeySpec pubKeySpec = new X509EncodedKeySpec(content);
+        // PublicKey generatePublic = factory.generatePublic(pubKeySpec);
+
+        // copy from jwtservice
+        KeyFactory factory = KeyFactory.getInstance("RSA");
+        ClassPathResource publicKeyResource = new ClassPathResource(PUBLIC_KEY_PATH);
+        try (FileReader keyReader = new FileReader(publicKeyResource.getFile());
+        PemReader pemReader = new PemReader(keyReader)) {
+
+            PemObject pemObject = pemReader.readPemObject();
+            byte[] content = pemObject.getContent();
+            X509EncodedKeySpec pubKeySpec = new X509EncodedKeySpec(content);
+            PublicKey generatePublic = factory.generatePublic(pubKeySpec);
+            return NimbusReactiveJwtDecoder.withPublicKey((RSAPublicKey) generatePublic).build();
+        }
+	}
+
+    @Bean
+    JWKSource<SecurityContext> jwkSource() {
+        return new JWKSource<SecurityContext>() {
+
+            @Override
+            public List<JWK> get(JWKSelector arg0, SecurityContext arg1) throws KeySourceException {
+                
+                try {
+                    ClassPathResource publicKeyResource = new ClassPathResource(PUBLIC_KEY_PATH);
+                    FileReader keyReader;
+                    PemObject pemObject;
+                    keyReader = new FileReader(publicKeyResource.getFile());
+                    PemReader pemReader = new PemReader(keyReader);
+                    pemObject = pemReader.readPemObject();
+                    byte[] content = pemObject.getContent();
+                    JWK parse = JWK.parse(new String(content));
+                    List<JWK> result = Arrays.asList(parse);
+                    return result;
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (ParseException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                return null;
+            }            
+        };
     }
 }
